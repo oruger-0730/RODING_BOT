6ff7fc29b0d52c45a438b2b8590dc9eee90be92b
Amur <135433478+Amur-0713@users.noreply.github.com>
2024-09-01 05:02:40 +0000

    ログイン

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const { Client, Intents } = require("discord.js");
-require("dotenv").config();
-const config = require("./config.json");
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Client({
-    intents: [
-        intents.FLAGS.GUILDS,
-        intents.FLAGS.GUILDS_MEMBERS,
-        intents.FLAGS.GUILDS_INVITES,
-        intents.FLAGS.GUILDS_MESSAGES
-    ],
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`${readyClient.user.tag}にログインしました`);
 });
+
+client.login(token);
