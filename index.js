e7c8c5b59cae8ba00af8b51bf1ab682d7ee98884
Amur <135433478+Amur-0713@users.noreply.github.com>
2024-09-03 02:41:48 +0000

    変更

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-const { REST, Routes,Client, Events, GatewayIntentBits } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+const { Client, Collection, GatewayIntentBits,REST, Routes } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
-
-const commands = [];
-
-// コマンドフォルダを取得
+client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    // 各フォルダ内のコマンドファイルを取得
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-    // 各コマンドのデータをJSON形式で取得して配列に追加
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.log(`[警告] ${filePath} にあるコマンドは、"data" または "execute" プロパティが不足しています。`);
-        }
-    }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
-// RESTモジュールのインスタンスを作成してトークンを設定
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 const rest = new REST().setToken(token);
-
-// コマンドをデプロイ
+// and deploy your commands!
 (async () => {
-    try {
-        console.log(`アプリケーションのスラッシュコマンド${commands.length}件の更新を開始します。`);
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // putメソッドを使ってギルド内のすべてのコマンドを現在のセットで更新
-        const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-        console.log(`アプリケーションのスラッシュコマンド${data.length}件を正常に再読み込みしました。`);
-    } catch (error) {
-        // エラーハンドリング
-        console.error(error);
-    }
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
 })();
 
 client.login(token);
